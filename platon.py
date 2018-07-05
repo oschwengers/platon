f751aaca340dbcfa0bfeeb72a3b5ffedb0146745
Oliver Schwengers <oliver@schwengers.de>
2018-07-05 16:10:54 +0200

    init version 0.1

---
+++ platon.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python3
+
+import argparse
+import functools as ft
+import json
+import multiprocessing as mp
+import os
+import re
+import sys
+import shutil
+import subprocess as sp
+import tempfile
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+
+from Bio import SeqIO
+
+PLATON_HOME = os.getenv( 'PLATON_HOME', None )
+if( PLATON_HOME is None ): sys.exit( 'ERROR: PLATON_HOME not set!' )
+
+from platon_functions import *
+
+
+# parse arguments
+parser = argparse.ArgumentParser( prog='platon',
+    description='PLAsmid conTig extractiOn' )
+parser.add_argument( 'genome', metavar='<genome>', help='Draft genome in fasta format' )
+parser.add_argument( '--threads', '-t', action='store', type=int, default=mp.cpu_count(), help='Number of threads to use (default = number of available CPUs)' )
+parser.add_argument( '--verbose', '-v', action='store_true', help='Print verbose information' )
+parser.add_argument( '--output', '-o', help='Output directory' )
+parser.add_argument( '--version', action='version', version='%(prog)s 0.1' )
+args = parser.parse_args()
+
+
+# global constants
+MIN_PROTEIN_IDENTITY = 80.0
+MIN_PROTEIN_SCORE_THRESHOLD = -18.4  # counts for sensitivity => 99 %
+PROTEIN_SCORE_PENALTY = -1.0
+SPADES_CONTIG_PATTERN = re.compile( 'NODE_\d+_length_\d+_cov_(\d+\.\d+)' )
+
+
+# check parameters & environment variables
+if( args.verbose ): print( 'Options, parameters and arguments:' )
+
+
+PLATON_HOME = os.path.abspath( PLATON_HOME )
+if( not os.access( PLATON_HOME, os.R_OK & os.X_OK ) ): sys.exit( 'ERROR: PLATON_HOME ('+PLATON_HOME+') not readable/accessible!' )
+if( args.verbose ): print( '\tPLATON_HOME: ' + PLATON_HOME )
+
+genomePath = os.path.abspath( args.genome )
+if( not os.access( genomePath, os.R_OK ) ): sys.exit( 'ERROR: genome file not readable!' )
+if( args.verbose ): print( '\tgenome path: ' + genomePath )
+
+outputPath = os.path.abspath(args.output) if args.output else os.getcwd()
+if( args.verbose ): print( '\toutput path: ' + outputPath )
+
+tmpPath = tempfile.mkdtemp()
+if( args.verbose ): print( '\ttmp path: ' + tmpPath )
+
+if( args.verbose ): print( '\t# threads: ' + str(args.threads) )
+
+fhFNULL = open( os.devnull, 'w' )
+
+
+# parse draft genome
+if( args.verbose ): print( 'parse draft genome...' )
+contigs = {}
+rawContigs = []
+filteredDraftGenomePath = tmpPath + '/genome.fasta'
+with open( filteredDraftGenomePath, 'w' ) as fh:
+    for record in SeqIO.parse( genomePath, 'fasta' ):
+
+        contig = {
+            'id': record.id,
+            'length': len(record.seq),
+            'sequence': str(record.seq),
+            'orfs': {},
+            'inc_types': [],
+            'mobilization_hits': [],
+            'replication_hits': [],
+            'conjugation_hits': [],
+            'rrnas': [],
+            'plasmid_hits': []
+        }
+
+        rawContigs.append(contig)
+
+        # read coverage from contig names if they were assembled with SPAdes
+        match = re.fullmatch( SPADES_CONTIG_PATTERN, record.id )
+        contig['coverage'] = 0 if (match is None) else float( match.group(1) )
+
+        # only include contigs with reasonable lengths
+        if( contig['length'] >= 1000  and  contig['length'] < 500000 ):
+            contigs[ record.id ] = contig
+            fh.write( '>' + contig['id'] + '\n' )
+            fh.write( contig['sequence'] + '\n' )
+        else:
+            if( args.verbose ):
+                if( contig['length'] < 1000 ):
+                    print( '\texclude contig \'%s\', too short (%d)' % (contig['id'], contig['length']) )
+                elif( contig['length'] >= 500000 ):
+                    print( '\texclude contig \'%s\', too long (%d)' % (contig['id'], contig['length']) )
+
+
+# predict ORFs
+if( args.verbose ): print( 'predict ORFs...' )
+proteinsPath = predict_orfs( tmpPath, contigs, filteredDraftGenomePath )
+if( args.verbose ):
+    noOrfs = ft.reduce( lambda x,y: x+y , map(lambda k: len(contigs[k]['orfs']), contigs))
+    print( '\tfound %d ORFs' % noOrfs )
+
+
+# exclude contigs without ORFs
+tmpContigs = {}
+for contig in filter( lambda k: len(k['orfs']) > 0, contigs.values() ):
+    tmpContigs[ contig['id'] ] = contig
+if( args.verbose ):
+    noRemovedContigs = len(contigs) - len(tmpContigs)
+    print( '\tremoved %d contig(s), no ORFs found' % (noRemovedContigs) )
+contigs = tmpContigs
+
+
+# find marker genes
+if( args.verbose ): print( 'search marker proteins...' )
+outPath = tmpPath + '/ghostz.tsv'
+sp.check_call( [ PLATON_HOME + '/share/ghostz',
+        'aln',
+        '-d', PLATON_HOME + '/db/refseq-bacteria-nrpc-reps',
+        '-b', '1', # max 1 result per query
+        '-a', str(args.threads), # threads
+        '-i', proteinsPath,
+        '-o', outPath
+    ],
+    cwd = tmpPath,
+    stdout = fhFNULL,
+    stderr = sp.STDOUT
+)
+
+
+# parse ghostz output
+noProteinsIdentified = 0
+with open( outPath, 'r' ) as fh:
+        for line in fh:
+            cols = line.split( '\t' )
+            locus = cols[0].split( ' ' )[0].rpartition( '_' )
+            if( float(cols[2]) >= MIN_PROTEIN_IDENTITY ):
+                contig = contigs[ locus[0] ]
+                orf = contig['orfs'][ locus[2] ]
+                orf['protein_id'] = cols[1]
+                noProteinsIdentified += 1
+if( args.verbose ): print( '\tfound %d marker proteins' % (noProteinsIdentified) )
+
+
+# parse protein score file
+if( args.verbose ): print( 'score and prefilter contigs...' )
+markerProteins = {}
+with open( PLATON_HOME + '/db/protein-scores.tsv', 'r' ) as fh:
+    for line in fh:
+        cols = line.split( '\t' )
+        markerProteins[ cols[0] ] = {
+            'id': cols[0],
+            'product': cols[1],
+            'length': cols[2],
+            'score': float(cols[3]),
+        }
+
+
+# calculate protein score per contig
+for contig in contigs.values():
+    scoreSum = 0.0
+    for orf in contig['orfs'].values():
+        if( ('protein_id' in orf)  and  (orf['protein_id'] in markerProteins) ):
+            markerProtein = markerProteins[ orf['protein_id'] ]
+            score = markerProtein['score']
+            orf['score'] = score
+            orf['product'] = markerProtein['product']
+            scoreSum += score
+        else:
+            scoreSum += PROTEIN_SCORE_PENALTY
+    contig['protein_score'] = scoreSum / len(contig['orfs']) if len(contig['orfs']) > 0 else 0
+
+
+# filter contigs based on conservative protein score threshold
+# MIN_PROTEIN_SCORE_THRESHOLD and execute per contig analyses in parallel
+scoredContigs = { k:v for (k,v) in contigs.items() if v['protein_score'] >= MIN_PROTEIN_SCORE_THRESHOLD }
+if( args.verbose ):
+    noExcludedContigs = len(contigs) - len(scoredContigs)
+    print( '\texcluded %d contigs by min protein score threshold (%2.1f)' % (noExcludedContigs, MIN_PROTEIN_SCORE_THRESHOLD) )
+    print( 'analyze contigs...' )
+
+
+# extract proteins from potential plasmid contigs for subsequent analyses
+filteredProteinsPath = tmpPath + '/proteins-filtered.faa'
+with open( filteredProteinsPath, 'w' ) as fh:
+    for record in SeqIO.parse( proteinsPath, 'fasta' ):
+        orfName = str(record.id).split()[0]
+        contigId = orfName.rsplit('_', 1 )[0]
+        if( contigId in scoredContigs ):
+            fh.write( '>' + orfName + '\n' )
+            fh.write( str(record.seq) + '\n' )
+
+
+# write contig sequences to fasta files for subsequent parallel analyses
+for cId, contig in scoredContigs.items():
+    contigPath = tmpPath + '/' + contig['id'] + '.fasta'
+    with open( contigPath, 'w' ) as fh:
+        fh.write( '>' + contig['id'] + '\n' )
+        fh.write( contig['sequence'] + '\n' )
+
+
+# init thread pool to parallize io bound analyses
+with ThreadPoolExecutor( max_workers=args.threads ) as tpe:
+    # start search for replication, mobilization and conjugation genes
+    for fn in (search_replication_genes, search_mobilization_genes, search_conjugation_genes):
+        tpe.submit( fn, tmpPath, scoredContigs, filteredProteinsPath )
+    # analyse contigs
+    for cId, contig in scoredContigs.items():
+        tpe.submit( test_circularity, tmpPath, contig )
+        tpe.submit( search_inc_type, tmpPath, contig )
+        tpe.submit( search_rrnas, tmpPath, contig )
+        tpe.submit( search_reference_plasmids, tmpPath, contig )
+
+
+# remove tmp dir
+shutil.rmtree( tmpPath )
+fhFNULL.close()
+
+
+# filter contigs
+filteredContigs = {k:v for (k,v) in scoredContigs.items() if filter_contig(v) }
+
+
+# get file prefix
+prefix = Path(genomePath).name
+if( '.' in prefix ): prefix = prefix.split('.')[0]
+
+
+# print results to tsv file and STDOUT
+header = 'ID\tLength\tCoverage\t# ORFs\tProtein Score\tCircular\tInc Type(s)\t# Replication\t# Mobilization\t# Conjugation\t# rRNAs\t# Plasmid Hits'
+print( header )
+outPath = outputPath + '/' + prefix + '.tsv'
+with open( outPath, 'w' ) as fh:
+    fh.write( header + '\n' )
+    for cId in sorted(filteredContigs, key=lambda k: -filteredContigs[k]['length']):
+        c = filteredContigs[cId]
+        line = '%s\t%d\t%4.1f\t%d\t%3.1f\t%s\t%s\t%d\t%d\t%d\t%d\t%d' % (c['id'], c['length'], c['coverage'], len(c['orfs']), c['protein_score'], 'yes' if c['is_circular'] else 'no', c['inc_types'][0]['type'] if len(c['inc_types'])>0 else '-', len(c['replication_hits']), len(c['mobilization_hits']), len(c['conjugation_hits']), len(c['rrnas']), len(c['plasmid_hits']))
+        print( line )
+        fh.write( line + '\n' )
+
+
+# write comprehensive results to JSON file
+outPath = outputPath + '/' + prefix + '.json'
+with open( outPath, 'w' ) as fh:
+    indent = '\t' if args.verbose else None
+    separators = ( ', ', ': ' ) if args.verbose else ( ',', ':' )
+    json.dump( filteredContigs, fh, indent=indent, separators=separators )
+
+
+# write chromosome contigs to fasta file
+outPath = outputPath + '/' + prefix + '.chromosome.fasta'
+with open( outPath, 'w' ) as fh:
+    for contig in rawContigs:
+        if( not contig['id'] in filteredContigs ):
+            fh.write( '>' + contig['id'] + '\n' )
+            fh.write( contig['sequence'] + '\n' )
+
+
+# write plasmid contigs to fasta file
+outPath = outputPath + '/' + prefix + '.plasmid.fasta'
+with open( outPath, 'w' ) as fh:
+    for contig in rawContigs:
+        if( contig['id'] in filteredContigs ):
+            fh.write( '>' + contig['id'] + '\n' )
+            fh.write( contig['sequence'] + '\n' )
---
+++ platon_functions.py
@@ -0,0 +1,394 @@
+
+import os
+import subprocess as sp
+
+
+# global constants
+PROTEIN_SCORE_CONSERVATIVE_THRESHOLD = -1  # counts for specificity >= 95 %
+PROTEIN_SCORE_TRUSTED_THRESHOLD = 1.4  # counts for specificity >= 99.99 %
+
+
+# read environment variables and set helper variables
+PLATON_HOME = os.path.abspath( os.getenv( 'PLATON_HOME', None ) )
+
+fhFNULL = open( os.devnull, 'w' )
+
+
+# test for circularity
+def test_circularity( tmpPath, contig ):
+
+    contigSplitPosition = int(contig['length'] / 2)
+
+    contigFragmentAPath = tmpPath + '/' + contig['id'] + '-a.fasta'
+    with open( contigFragmentAPath, 'w' ) as fh:
+        fh.write( '>a\n')
+        fh.write( contig['sequence'][:contigSplitPosition] + '\n ')
+
+    contigFragmentBPath = tmpPath + '/' + contig['id'] + '-b.fasta'
+    contigFragmentBSequence = contig['sequence'][contigSplitPosition:]
+    with open( contigFragmentBPath, 'w' ) as fh:
+        fh.write( '>b\n ')
+        fh.write( contigFragmentBSequence + '\n ')
+
+    sp.check_call( [ PLATON_HOME + '/share/nucmer',
+            '-f', # only forward strand
+            '-l', '40', # increase min match length to 40 bp
+            '--threads=1',
+            '-p', contig['id'],
+            contigFragmentBPath,
+            contigFragmentAPath
+        ],
+        cwd = tmpPath,
+        stdout = fhFNULL,
+        stderr = sp.STDOUT
+    )
+
+    contig['is_circular'] = False
+    hasMatch = False
+    with open( tmpPath + '/' + contig['id'] + '.delta', 'r' ) as fh:
+        for line in fh:
+            line = line.rstrip()
+            if( line[0] == '>' ):
+                hasMatch = True
+            elif( hasMatch ):
+                cols = line.split( ' ' )
+                if( len(cols) == 7 ):
+                    bStart = int(cols[0])
+                    bEnd   = int(cols[1])
+                    aStart = int(cols[2])
+                    aEnd   = int(cols[3])
+                    mismatches = int(cols[4])
+                    aAlignment = aEnd - aStart + 1
+                    bAlignment = bEnd - bStart + 1
+                    if( aAlignment == bAlignment  and  aAlignment > 50
+                        and (mismatches/aAlignment) < 0.05
+                        and bEnd == len(contigFragmentBSequence)
+                        and aStart == 1):
+                            contig['is_circular'] = True
+                            contig['circular_link'] = {
+                                'length': aAlignment,
+                                'mismatches': mismatches,
+                                'prime5Start': 1,
+                                'prime5End': aAlignment,
+                                'prime3Start': contig['length'] - bAlignment + 1,
+                                'prime3End': contig['length'],
+                            }
+                            break
+    return
+
+
+
+
+# search for inc type signatures
+def search_inc_type( tmpPath, contig ):
+
+    contigPath = tmpPath + '/' + contig['id'] + '.fasta'
+    outPath = tmpPath + '/' + contig['id'] + '.inc.blast.out'
+    sp.check_call( [ PLATON_HOME + '/share/blastn',
+            '-query', PLATON_HOME + '/db/inc-types.fasta',
+            '-subject', contigPath,
+            '-num_threads', '1',
+            '-perc_identity', '80',
+            '-culling_limit', '1',
+            '-outfmt', '6 qseqid sstart send sstrand pident qcovs',
+            '-out', outPath
+        ],
+        cwd = tmpPath,
+        stdout = fhFNULL,
+        stderr = sp.STDOUT
+    )
+
+    with open( outPath, 'r' ) as fh:
+        for line in fh:
+            cols = line.rstrip().split( '\t' )
+            hit = {
+                'type': cols[0],
+                'start': int(cols[1]),
+                'end': int(cols[2]),
+                'strand': '+' if cols[3] == 'plus' else '-',
+                'identity': float(cols[4]) / 100,
+                'coverage': float(cols[5]) / 100
+            }
+            if( hit['coverage'] >= 0.9 ):
+                contig['inc_types'].append( hit )
+    return
+
+
+
+
+# search for rRNAs
+def search_rrnas( tmpPath, contig ):
+
+    contigPath = tmpPath + '/' + contig['id'] + '.fasta'
+    outPath = tmpPath + '/' + contig['id'] + '.rrna.cmscan.tsv'
+    sp.check_call( [ PLATON_HOME + '/share/cmscan',
+            '--noali',
+            '--cut_tc',
+            '--cpu', '1',
+            '--tblout', outPath,
+            PLATON_HOME + '/db/rRNA',
+            contigPath,
+        ],
+        cwd = tmpPath,
+        stdout = fhFNULL,
+        stderr = sp.STDOUT
+    )
+
+    with open( outPath, 'r' ) as fh:
+        for line in fh:
+            if( line[0] != '#' ):
+                cols = line.rstrip().split()
+                hit = {
+                    'type': cols[0],
+                    'start': int(cols[7]),
+                    'end': int(cols[8]),
+                    'strand': cols[9],
+                    'bitscore': float(cols[14]),
+                    'evalue': float(cols[15])
+                }
+                contig['rrnas'].append( hit )
+    return
+
+
+
+
+# search for reference plasmid hits
+def search_reference_plasmids( tmpPath, contig ):
+
+    # reduce blastn word size to overcome segmentation faults due to too many
+    # HSPs. As filtered contigs are at least 1k bp long, word size cannot be
+    # smaller than 10.
+    blastWordSize = int( contig['length'] / 100 )
+
+    contigPath = tmpPath + '/' + contig['id'] + '.fasta'
+    outPath = tmpPath + '/' + contig['id'] + '.refplas.blast.out'
+    sp.check_call( [ PLATON_HOME + '/share/blastn',
+            '-query', contigPath,
+            '-db', PLATON_HOME + '/db/refseq-plasmids',
+            '-num_threads', '1',
+            '-culling_limit', '1',
+            '-perc_identity', '80',
+            '-word_size', str(blastWordSize),
+            '-outfmt', '6 sseqid qstart qend sstart send slen length nident',
+            '-out', outPath
+        ],
+        cwd = tmpPath,
+        stdout = fhFNULL,
+        stderr = sp.STDOUT
+    )
+
+    with open( outPath, 'r' ) as fh:
+        for line in fh:
+            line = line.rstrip()
+            cols = line.split( '\t' )
+            hit = {
+                'contig_start': int(cols[1]),
+                'contig_end': int(cols[2]),
+                'plasmid_start': int(cols[3]),
+                'plasmid_end': int(cols[4]),
+                'plasmid': {
+                    'id': cols[0],
+                    'length': int(cols[5])
+                },
+                'coverage': float(cols[6]) / contig['length'],
+                'identity': float(cols[7]) / float(cols[6])
+            }
+            if( hit['coverage'] >= 0.8 ):
+                contig['plasmid_hits'].append( hit )
+    return
+
+
+
+
+def filter_contig( contig ):
+
+    # include all circular contigs
+    if( contig['is_circular'] ):
+        return True
+
+    # include all contigs containing Inc type signatures
+    if( len(contig['inc_types']) > 0 ):
+        return True
+
+    # include all contigs containing replication genes
+    if( len(contig['replication_hits']) > 0 ):
+        return True
+
+    # include all contigs containing mobilization genes
+    if( len(contig['mobilization_hits']) > 0 ):
+        return True
+
+    # include all contigs with high confidence protein scores
+    if( contig['protein_score'] > PROTEIN_SCORE_TRUSTED_THRESHOLD ):
+        return True
+
+    # include all contigs with mediocre protein scores but additional blast hit evidence without rRNAs
+    if( contig['protein_score'] > PROTEIN_SCORE_CONSERVATIVE_THRESHOLD  and  len(contig['plasmid_hits']) > 0  and  len(contig['rrnas']) == 0 ):
+        return True
+
+    return False
+
+
+
+
+# predict ORFs
+def predict_orfs( tmpPath, contigs, filteredDraftGenomePath ):
+    proteinsPath = tmpPath + '/proteins.faa'
+    gffPath = tmpPath + '/prodigal.gff'
+    sp.check_call( [ PLATON_HOME + '/share/prodigal',
+            '-i', filteredDraftGenomePath,
+            '-a', proteinsPath,
+            '-c', # closed ends
+            '-f', 'gff', # GFF output
+            '-o', gffPath # prodigal output
+        ],
+        cwd = tmpPath,
+        stdout = fhFNULL,
+        stderr = sp.STDOUT
+    )
+
+    # parse orfs
+    with open( gffPath, 'rU' ) as fh:
+        for line in fh:
+            if( line[0] != '#' ):
+                cols = line.split( '\t' )
+                orfId = cols[8].split(';')[0].split('=')[1].split('_')[1]
+                orf = {
+                    'start': cols[3],
+                    'end': cols[4],
+                    'strand': cols[6],
+                    'id': orfId
+                }
+                contig = contigs[ cols[0] ]
+                if( contig is not None ):
+                    contig['orfs'][ orfId ] = orf
+    return proteinsPath
+
+
+
+
+# search for replication genes
+def search_replication_genes( tmpPath, contigs, filteredProteinsPath ):
+
+    outPath = tmpPath + '/rep.hmm.out'
+    sp.check_call( [ PLATON_HOME + '/share/hmmscan',
+            '--noali',
+            '--cpu', '1',
+            '-E', '1E-100',
+            '--tblout', outPath,
+            PLATON_HOME + '/db/replication',
+            filteredProteinsPath
+        ],
+        cwd = tmpPath,
+        stdout = fhFNULL,
+        stderr = sp.STDOUT
+    )
+
+    hits = set()
+    with open( outPath, 'r' ) as fh:
+        for line in fh:
+            if( line[0] != '#' ):
+                cols = line.rstrip().split()
+                if( not cols[2] in hits ):
+                    tmp = cols[2].rsplit('_', 1 )
+                    contigId = tmp[0]
+                    contig = contigs[ contigId ]
+                    orf = contig['orfs'][tmp[1]]
+                    hit = {
+                        'type': cols[0],
+                        'start': orf['start'],
+                        'end': orf['end'],
+                        'strand': orf['strand'],
+                        'bitscore': float(cols[5]),
+                        'evalue': float(cols[4])
+                    }
+                    hits.add( cols[2] )
+                    contig['replication_hits'].append(hit)
+
+    return
+
+
+
+
+# search for mobilization genes
+def search_mobilization_genes( tmpPath, contigs, filteredProteinsPath ):
+
+    outPath = tmpPath + '/mob.hmm.out'
+    sp.check_call( [ PLATON_HOME + '/share/hmmscan',
+            '--noali',
+            '--cpu', '1',
+            '-E', '1E-100',
+            '--tblout', outPath,
+            PLATON_HOME + '/db/mobilization',
+            filteredProteinsPath
+        ],
+        cwd = tmpPath,
+        stdout = fhFNULL,
+        stderr = sp.STDOUT
+    )
+
+    hits = set()
+    with open( outPath, 'r' ) as fh:
+        for line in fh:
+            if( line[0] != '#' ):
+                cols = line.rstrip().split()
+                if( not cols[2] in hits ):
+                    tmp = cols[2].rsplit('_', 1 )
+                    contigId = tmp[0]
+                    contig = contigs[ contigId ]
+                    orf = contig['orfs'][tmp[1]]
+                    hit = {
+                        'type': cols[0],
+                        'start': orf['start'],
+                        'end': orf['end'],
+                        'strand': orf['strand'],
+                        'bitscore': float(cols[5]),
+                        'evalue': float(cols[4])
+                    }
+                    hits.add( cols[2] )
+                    contig['mobilization_hits'].append(hit)
+
+    return
+
+
+
+
+# search for conjugation genes
+def search_conjugation_genes( tmpPath, contigs, filteredProteinsPath ):
+
+    outPath = tmpPath + '/conj.hmm.out'
+    sp.check_call( [ PLATON_HOME + '/share/hmmscan',
+            '--noali',
+            '--cpu', '1',
+            '-E', '1E-100',
+            '--tblout', outPath,
+            PLATON_HOME + '/db/conjugation',
+            filteredProteinsPath
+        ],
+        cwd = tmpPath,
+        stdout = fhFNULL,
+        stderr = sp.STDOUT
+    )
+
+    hits = set()
+    with open( outPath, 'r' ) as fh:
+        for line in fh:
+            if( line[0] != '#' ):
+                cols = line.rstrip().split()
+                if( not cols[2] in hits ):
+                    tmp = cols[2].rsplit('_', 1 )
+                    contigId = tmp[0]
+                    contig = contigs[ contigId ]
+                    orf = contig['orfs'][tmp[1]]
+                    hit = {
+                        'type': cols[0],
+                        'start': orf['start'],
+                        'end': orf['end'],
+                        'strand': orf['strand'],
+                        'bitscore': float(cols[5]),
+                        'evalue': float(cols[4])
+                    }
+                    hits.add( cols[2] )
+                    contig['conjugation_hits'].append(hit)
+
+    return
