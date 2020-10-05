79aac56066ccb65bda97f10bcc649afc43698529
oschwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2020-10-05 16:14:42 +0200

    update RDS thresholds to new database

--- platon/constants.py
+++ platon/constants.py
@@ -4,9 +4,9 @@ import re
 MIN_CONTIG_LENGTH = 1000
 MAX_CONTIG_LENGTH = 500000
 MIN_PROTEIN_IDENTITY = 90.0
-RDS_SENSITIVITY_THRESHOLD = -7.7  # sensitivity => 95 %
-RDS_CONSERVATIVE_THRESHOLD = 0.1  # (-0.9) specificity >= 95 %
-RDS_SPECIFICITY_THRESHOLD = 0.4  # (-0.5) specificity >= 99.9 %
+RDS_SENSITIVITY_THRESHOLD = -7.9  # sensitivity => 95 %
+RDS_CONSERVATIVE_THRESHOLD = 0.1  # highest accuracy
+RDS_SPECIFICITY_THRESHOLD = 0.7  # specificity >= 99.9 %
 MIN_CIRC_BASEPAIR_OVERLAP = 100
 
 SPADES_CONTIG_PATTERN = re.compile(r'NODE_\d+_length_\d+_cov_(\d+\.\d+)')
