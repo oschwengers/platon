44227f7753f4f5ea8ae899fc02724636a3ec74e4
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2021-12-07 16:28:48 +0100

    discard support for Python < 3.8

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
     classifiers=[
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
