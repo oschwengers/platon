24afd90479101002a34280ae5dce8bb770dfd783
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2023-03-03 15:37:02 +0100

    fix collect inc type aux script

--- aux-scripts/collect-inctypes.py
+++ aux-scripts/collect-inctypes.py
@@ -28,13 +28,7 @@ for result_file in args.results:
         with result_path.open() as fh:
             content = json.load(fh)
         for contig_id, value in content.items():
-            # for inc_type in value.get('inc_types', []):
             for inc_type in value.get('plasmid_hits', []):
-                
-                # [{"type":"IncFIB(K)_1_JN233704","start":1,"end":559,"strand":"+","identity":0.98929,"coverage":1.0,"bitscore":1002}]
-                # inc_type = inc_type['type']
-
-                # [{"contig_start":1,"contig_end":1475,"plasmid_start":263545,"plasmid_end":265019,"plasmid":{"id":"NC_012556.1","length":324503},"coverage":1.0,"identity":1.0}]
                 inc_type = inc_type['plasmid']['id']
                 inc_types.append(inc_type)
                 global_inc_types.add(inc_type)
@@ -43,7 +37,9 @@ for result_file in args.results:
         print(f'could not read file: {result_file}')
 
 global_inc_types = sorted(list(global_inc_types))
-print(f"Sample\t{'\t'.join(global_inc_types)}")
+tmp = '\t'.join(global_inc_types)
+print(f"Sample\t{tmp}")
 for sample, inc_types in sample_inc_types.items():
     inc_type_presence = ['1' if i in inc_types else '0' for i in global_inc_types]
-    print(f"{sample}\t{'\t'.join(inc_type_presence)}")
+    tmp = '\t'.join(inc_type_presence)
+    print(f"{sample}\t{tmp}")
