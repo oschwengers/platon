0264a84535a5f1385939e3b46a7adfbe40309b36
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2023-08-21 16:29:25 +0200

    add support for Python v3.11

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
